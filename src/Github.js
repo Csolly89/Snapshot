3aecc72acc7105db4025038fb4774ba4c798e7b6
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-12-08 17:34:07 -0500

    test's have passed

--- src/Github.js
+++ src/Github.js
@@ -1,9 +1,18 @@
-
+import Card from 'react-bootstrap/Card';
+import FamilyImg from './Family.jpg'
 
 function Github () {
     return (
         <div>
-
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={FamilyImg} />
+                <Card.Body>
+                    <Card.Title>For Family</Card.Title>
+                    <Card.Text>
+                        Nothings impossible when you have Family - Dom
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
